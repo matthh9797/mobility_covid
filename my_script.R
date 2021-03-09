1cf3fd4a7a71508fc604411869bd697a350e41f3
Matthew Henderson <matthewh@netextremity.com>
2021-03-09 16:58:08 +0000

    Change the initial download of data in the main script to be reproducable and rewrote the initial pre-processing

--- my_script.R
+++ my_script.R
@@ -1,4 +1,4 @@
-# Modelling Correlation of Mobility
+# Modeling Correlation of Mobility
 # with rates of Coronavirus
 
 # libraries
@@ -7,48 +7,79 @@ library(dplyr)
 library(reshape2)
 library(lubridate)
 library(covid19.analytics)
-
-# initial data download
-# google mobility data (422.4 MB, Sys.Date = "2021-03-09")
-if (!dir.exists("data")) {
-    # create data directory 
-    dir.create("data")
-}
-# url of the csv download 
-url <- "https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv"
-# download file into data directory
-download.file(url, destfile = "data/google_global_mobility_report.csv")
+library(data.table)
+library(dtplyr)
 
 
-# load the data ...
-# covid data from John Hopkins 
-covid <- covid19.data(case = "ts-confirmed")
-# Mobility data from Google
-mobility <- read.csv("data/Global_Mobility_Report.csv")
+# 1. INITIAL DATA DOWNLOAD AND PRE-PROCESSING
 
-# Subset mobility data
-mobility <- mobility %>% 
-    # sub region "" why?
-    filter(sub_region_1 == "") %>% 
-    mutate(date = ymd(date)) %>% 
-    # dates before end of first wave
-    filter(date <= "2020-07-01") %>% 
-    # select columns
-    select(-country_region_code, -sub_region_1, -sub_region_2, -metro_area, -iso_3166_2_code,
-           -census_fips_code, -place_id)
+## I HAVE COMMENTED THIS CODE OUT AS IT SHOULD NOT NEED TO BE RUN AFTER THE 
+## INITIAL TIME !!!
 
-# subset the covid data
-covid <- covid %>% 
-    pivot_longer(col = -c("Province.State", "Country.Region", "Lat", "Long"),
-                                names_to = "date", values_to = "cumulative_cases") %>%
-    # Province.State == "" means just British mainland
-    filter(Province.State == "")
-    # dates before the first wave
-    mutate(date = ymd(date)) %>%
-    filter(date <= "2020-07-01") %>%
-    # after first case
-    filter(cumulative_cases > 0)
-        
+# # google mobility data (422.4 MB, Sys.Date = "2021-03-09")
+# if (!dir.exists("data")) {
+#     # create data directory
+#     dir.create("data")
+# }
+# # # url of the csv download
+# url <- "https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv"
+# # download file into data directory
+# download.file(url, destfile = "data/google_global_mobility_report.csv")
+# rm(url)
+
+
+# # mobility data from Google
+# # fread large df (4 million rows)
+# mobility <- fread("data/google_global_mobility_report.csv")
+# # covid data from John Hopkins
+# covid <- covid19.data(case = "ts-confirmed")
+# #
+# #
+# # Subset mobility data
+# # use dtplyr to convert the dplyr code to data.table for speed
+# mobility <- dtplyr::lazy_dt(mobility)
+# mobility <- mobility %>%
+#     # sub region "" means
+#     filter(sub_region_1 == "") %>%
+#     mutate(date = ymd(date)) %>%
+#     # dates before end of first wave
+#     filter(date <= "2020-07-01") %>%
+#     # select columns
+#     select(-country_region_code, -sub_region_1, -sub_region_2, -metro_area, -iso_3166_2_code,
+#            -census_fips_code, -place_id)
+# # #
+# # show data.table equivelant code
+# mobility %>% show_query()
+# # convert back to tibble
+# mobility <- mobility %>% as_tibble()
+# #
+# # # subset the covid data
+# covid <- covid %>%
+#     pivot_longer(col = -c("Province.State", "Country.Region", "Lat", "Long"),
+#                                 names_to = "date", values_to = "cumulative_cases") %>%
+#     # Province.State == "" means just British mainland
+#     filter(Province.State == "") %>%
+#     # dates before the first wave
+#     mutate(date = ymd(date)) %>%
+#     mutate(date = date(date)) %>%
+#     filter(date <= "2020-07-01") %>%
+#     # after first case
+#     filter(cumulative_cases > 0) %>%
+#     select(-Province.State, -Lat, -Long)
+# 
+# # Write subsetted dataframes to csv files for quick load
+# write.csv(mobility, "data/mobility_subset.csv", row.names = FALSE)
+# write.csv(covid, "data/covid_subset.csv", row.names = FALSE)
+
+
+# 2. wORKING WITH THE SUBSETTED DATA
+
+# read the subsetted data
+covid <- read_csv("data/covid_subset.csv")
+mobility <- read_csv("data/mobility_subset.csv")
+
+
+
 # more on data manipulation
 covid <- covid %>% mutate(week = week(date)) %>% filter(week > 8)
 mobility <- mobility %>% mutate(week = week(date)) %>% filter(week > 8)
